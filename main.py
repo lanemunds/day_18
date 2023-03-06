96e893c96c35304f57037fdc8adf51f87da36913
lanemunds <lanemunds@gmail.com>
2023-03-06 15:54:15 -0700

    dash line

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
+
+for turt in range(15):
+    timmy_the_turtle.forward(5)
+    timmy_the_turtle.pu()
+    timmy_the_turtle.forward(5)
+    timmy_the_turtle.pd()
+
 
 screen = Screen()
 screen.exitonclick()
