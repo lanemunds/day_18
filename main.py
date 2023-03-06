de614412b57da02710da0aa8742875a249183e64
lanemunds <lanemunds@gmail.com>
2023-03-06 16:37:28 -0700

    shape picture

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 from turtle import Turtle, Screen
+import random
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("green")
+color_list = ['red', 'green', 'blue', 'orange', 'black',
+              'yellow', 'brown', 'purple', 'pink', 'gold', 'silver', "gray"]
 
-for turt in range(15):
-    timmy_the_turtle.forward(5)
-    timmy_the_turtle.pu()
-    timmy_the_turtle.forward(5)
-    timmy_the_turtle.pd()
+timmy_the_turtle.color(random.choice(color_list))
+for turt in range(3):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(120)
 
+
+timmy_the_turtle.color(random.choice(color_list))
+for turt in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+timmy_the_turtle.color(random.choice(color_list))
+for turt in range(5):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(72)
+
+timmy_the_turtle.color(random.choice(color_list))
+for turt in range(6):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(60)
+
+timmy_the_turtle.color(random.choice(color_list))
+for turt in range(7):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(51.428)
+
+timmy_the_turtle.color(random.choice(color_list))
+for turt in range(8):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(45)
 
 screen = Screen()
 screen.exitonclick()
